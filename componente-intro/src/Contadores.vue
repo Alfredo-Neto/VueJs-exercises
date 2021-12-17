73c025bab0cf27a7d55374d529b7ed822af99e8b
Alfredo Neto <alfredoneto934@gmail.com>
2021-12-17 11:58:24 -0300

    added components-intro

---
+++ componente-intro/src/Contadores.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Contadores</h1>
+    <app-contador v-for="c in 5" :key="c"></app-contador>
+  </div>
+</template>
+
+<script>
+import Contador from "./Contador";
+export default {
+    components: {'app-contador': Contador}
+} 
+</script>
+
+<style>
+
+</style>
